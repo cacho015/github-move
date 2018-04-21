ad0da03d3501fdd633f6c7118d5ec8e917b6854a
juan <jgraneros47@gmail.com>
2018-04-21 02:06:08 -0300

    initializing repository

---
+++ src/Modelo/EquipoFutbol.java
@@ -0,0 +1,16 @@
+package Modelo;
+
+/**
+ *
+ * @author juan
+ */
+public class EquipoFutbol {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
